7d47c9d3504074319238d82e9301977042881089
Melissa Liebeck <melissaliebeck@aol.com>
2023-03-28 17:03:27 +0200

    push again because vercel issue and delete link from main

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import Heading from "../components/Heading";
-import Link from "next/link";
 export default function Home() {
   return (
     <main>
       <Heading>landwaerts</Heading>
-      <Link href="/rides-overview">Rides Overview</Link>
     </main>
   );
 }
