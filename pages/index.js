a90eb7ad9301117b0ddb9d292df5f3196ba277db
Melissa Liebeck <melissaliebeck@aol.com>
2023-03-29 14:58:36 +0200

    add list of rides to main page for vercel reasons

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Heading from "../components/Heading";
+import { rides } from "../lib/rides";
+import React from "react";
+import SecondHeading from "../components/SecondHeading";
+import List from "../components/List";
+
 export default function Home() {
   return (
     <main>
       <Heading>landwaerts</Heading>
+      <SecondHeading>Übersicht – Alle Fahrten</SecondHeading>
+      <List rides={rides} />
     </main>
   );
 }
